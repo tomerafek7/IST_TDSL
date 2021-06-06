8d83d04832ecafb52f0cdb1eebd8fe87f554799a
Maor <maor7934@gmail.com>
2021-06-06 14:55:50 +0300

    buildIdealISTree

--- src/main/java/ISTInnerNode.java
+++ src/main/java/ISTInnerNode.java
@@ -2,11 +2,36 @@ import java.util.ArrayList;
 
 public class ISTInnerNode<V> extends  ISTNode<V>{
     int numOfChildren;
-    int numOfLeaves;
+    int numOfLeaves; // used only in rebuild
     ArrayList<Integer> keys;
     ArrayList<ISTNode<V>> children;
     int updateCount;
     int activeTX;
     int waitQueueIndex;
+    ISTInnerNode(ArrayList<ISTSingleNode<V>> childrenList){
+        numOfLeaves = 0;
+        updateCount = 0;
+        activeTX = 0;
+        waitQueueIndex = 0;
+        numOfChildren = childrenList.size();
+        children = new ArrayList<ISTNode<V>>(childrenList);
+        //children = childrenList;
 
+        for (int i=1; i<childrenList.size(); i++){
+            keys.add(childrenList.get(i).key);
+
+        }
+        minKey = keys.get(0);
+        maxKey = keys.get(keys.size()-1);
+    }
+
+    public ISTInnerNode(int numOfChildrenReceived) {
+        numOfLeaves = 0;
+        updateCount = 0;
+        activeTX = 0;
+        waitQueueIndex = 0;
+        numOfChildren = numOfChildrenReceived;
+        keys = new ArrayList<Integer> ();
+        children = new ArrayList<ISTNode<V>> ();
+    }
 }
--- src/main/java/ISTRebuildObject.java
+++ src/main/java/ISTRebuildObject.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class ISTRebuildObject<V> {
     IST<V> oldIstTree;
@@ -6,11 +7,28 @@ public class ISTRebuildObject<V> {
     static final int MIN_TREE_LEAF_SIZE = 4; //TODO: might fight a better value
 
 
-    public ISTNode buildIdealISTree (ArrayList<ISTSingleNode<V>> KVPairList){
-        if (KVPairList.size()< MIN_TREE_LEAF_SIZE){
+    public ISTNode<V> buildIdealISTree (ArrayList<ISTSingleNode<V>> KVPairList){
+        int numOfLeaves = KVPairList.size();
+        if (numOfLeaves< MIN_TREE_LEAF_SIZE){
             ISTInnerNode<V> myNode = new ISTInnerNode<V>(KVPairList);
             myNode.children = new ArrayList<ISTNode<V>>();
             myNode.children.add(KVPairList.get(0));
+            return myNode;
         }
+        int numChildren = (int) Math.ceil(Math.sqrt(numOfLeaves));
+        int childSize = (int) Math.floor((((double)numOfLeaves)/numChildren));
+        int remainder = numOfLeaves%numChildren;
+        ISTInnerNode<V> myNode = new ISTInnerNode<V>(numChildren);
+        for (int i = 0; i<numChildren; i++){
+            int size = childSize + (i<remainder ? 1 : 0);
+            myNode.children.add(buildIdealISTree((ArrayList<ISTSingleNode<V>>) KVPairList.subList(0,size-1)));
+            if (i != 0){
+                myNode.keys.add(KVPairList.get(0).key);
+
+            }
+            KVPairList = (ArrayList<ISTSingleNode<V>>)KVPairList.subList(size,KVPairList.size()-1);
+        }
+        return myNode;
+
     }
 }
