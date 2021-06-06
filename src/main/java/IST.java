059ce131e3aa0da9a6d42bb477a679008ab26e6f
tomerafek7 <tomerafek7@gmail.com>
2021-06-06 12:15:02 +0300

    IST class WIP

--- src/main/java/IST.java
+++ src/main/java/IST.java
@@ -1,5 +1,33 @@
-public class IST {
+import java.util.Collections;
 
+public class IST <V> {
+
+    ISTNode<V> root;
+
+    public int interpolate(ISTInnerNode<V> node, Integer key){
+        Integer minKey = Collections.min(node.keys);
+        Integer maxKey = Collections.max(node.keys);
+        int numKeys = node.numOfChildren - 1;
+
+        // check boundaries:
+        if (key.compareTo(minKey) < 0) return 0;
+        else if (key.compareTo(maxKey) >= 0) return numKeys;
+
+        // estimate index:
+        int index = (numKeys * (key - minKey) / (maxKey - minKey));
+
+    }
+
+    public V lookup(Integer key){
+
+        ISTNode<V> curNode = root;
+        while (!(curNode instanceof ISTSingleNode)){
+            // checkAndHelpRebuild - TODO
+            int idx = interpolate((ISTInnerNode<V>) curNode, key);
+        }
+
+
+    }
 
 
 }
